6a884a9da022d2bbfe68753859aeb0d00c234e08
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-20 02:25:47 +0900

    swagger url 수정

--- sciq-be/src/main/java/com/guld/sciq/config/SwaggerConfig.java
+++ sciq-be/src/main/java/com/guld/sciq/config/SwaggerConfig.java
@@ -28,8 +28,7 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer"))
                 .servers(List.of(
-                        new Server().url("http://api.sciq.co.kr").description("Production server"),
-                        new Server().url("http://localhost:8080").description("localhost")));
+                        new Server().url("/api").description("Current server")));
     }
 }
 

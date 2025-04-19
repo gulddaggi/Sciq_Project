f453ba3e6de2ace7df7bc51f7f4d8c5e6c568fbb
Hyochang098 <wlsgyckd7@naver.com>
2025-04-20 05:17:09 +0900

    질문 이넘타입STATIC처리

--- sciq-be/src/main/java/com/guld/sciq/question/comment/dto/QuestionCommentCreateDto.java
+++ sciq-be/src/main/java/com/guld/sciq/question/comment/dto/QuestionCommentCreateDto.java
@@ -1,14 +1,31 @@
 package com.guld.sciq.question.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.guld.sciq.question.comment.entity.CommentType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Data
 @Builder
-public record QuestionCommentCreateDto(
-        @Schema(description = "댓글 내용", example = "좋은 질문이네요!")
-        String content,
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionCommentCreateDto {
+    @Schema(description = "댓글 내용", example = "좋은 질문이네요!")
+    private String content;
 
-        @Schema(description = "댓글 타입", example = "OPINION", implementation = CommentType.class)
-        CommentType commentType
-) {}
+    @Schema(description = "댓글 타입", example = "OPINION", implementation = CommentType.class)
+    private CommentType commentType;
+
+    /**
+     * 문자열 commentType을 CommentType enum으로 설정하는 메서드
+     * @param commentTypeStr 문자열 타입
+     */
+    @JsonProperty("commentType")
+    public void setCommentTypeFromString(String commentTypeStr) {
+        this.commentType = CommentType.fromString(commentTypeStr);
+    }
+}
--- sciq-be/src/main/java/com/guld/sciq/question/comment/entity/CommentType.java
+++ sciq-be/src/main/java/com/guld/sciq/question/comment/entity/CommentType.java
@@ -1,6 +1,29 @@
 package com.guld.sciq.question.comment.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum CommentType {
     OPINION,    // 학생(STUDENT)의 의견
-    ADVISE      // 조언자(ADVISOR)의 조언
+    ADVISE;     // 조언자(ADVISOR)의 조언
+
+    /**
+     * 문자열을 CommentType으로 변환합니다.
+     * 대소문자를 구분하지 않고 변환합니다.
+     * 
+     * @param value 변환할 문자열
+     * @return 변환된 CommentType 또는 값이 null이거나 일치하지 않을 경우 기본값(OPINION)
+     */
+    @JsonCreator
+    public static CommentType fromString(String value) {
+        if (value == null) {
+            return OPINION; // 기본값
+        }
+        
+        try {
+            return CommentType.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            // 일치하는 값이 없을 경우 기본값 반환
+            return OPINION;
+        }
+    }
 }

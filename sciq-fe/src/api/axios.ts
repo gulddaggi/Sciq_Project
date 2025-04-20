b13853b9d7c402eca875964960d32fbf6a69acea
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-20 11:20:01 +0900

    최종 시도

--- sciq-fe/src/api/axios.ts
+++ sciq-fe/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },

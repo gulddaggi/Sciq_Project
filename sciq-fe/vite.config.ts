360462e103149f1cea201dd95289d13d496774ca
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-21 14:07:28 +0900

    프록시 변경

--- sciq-fe/vite.config.ts
+++ sciq-fe/vite.config.ts
@@ -18,7 +18,6 @@ export default defineConfig(({ mode }) => {
         '/api': {
           target: env.VITE_API_BASE_URL,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
